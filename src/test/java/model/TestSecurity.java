1305b692b206ff2f3b6d547a8c93831d5e34137f
Shawn Aten <shawnmaten@gmail.com>
2017-06-20 16:14:33 -0500

    Add security class

---
+++ src/test/java/model/TestSecurity.java
@@ -0,0 +1,29 @@
+package model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Tests {@link Security Security} class to check encoding and signing.
+ */
+public class TestSecurity {
+    private static final Policy POLICY = new Policy.Builder().expiry(4653651600L).build();
+    private static final String APP_SECRET = "N3XOC2GP2NFTDCM43DZ6F2L6N4";
+    private static final String CORRECT_ENCODED_POLICY = "eyJleHBpcnkiOjQ2NTM2NTE2MDB9";
+    private static final String CORRECT_SIGNATURE = "d0dcacc68d00b7d2cd18c7f82aaf5bf172fdb423dc3cf0540d0da04912867e13";
+
+    @Test
+    public void TestEncoding() {
+        Security security = Security.createNew(POLICY, APP_SECRET);
+
+        assertTrue("Incorrect encoded policy ", CORRECT_ENCODED_POLICY.equals(security.getEncodedPolicy()));
+    }
+
+    @Test
+    public void TestSigning() {
+        Security security = Security.createNew(POLICY, APP_SECRET);
+
+        assertTrue("Incorrect signature", CORRECT_SIGNATURE.equals(security.getSignature()));
+    }
+}
