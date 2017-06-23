56ff114223e70c7093828c399749e6e506cee392
Shawn Aten <shawnmaten@gmail.com>
2017-06-23 15:43:54 -0500

    Add constructor with message to all Exceptions

--- src/main/java/exception/HandleNotFoundException.java
+++ src/main/java/exception/HandleNotFoundException.java
@@ -5,4 +5,8 @@ public class HandleNotFoundException extends FilestackIOException {
     public HandleNotFoundException() {
         super("No file with that handle was found");
     }
+
+    public HandleNotFoundException(String message) {
+        super(message);
+    }
 }
--- src/main/java/exception/PolicySignatureException.java
+++ src/main/java/exception/PolicySignatureException.java
@@ -5,4 +5,8 @@ public class PolicySignatureException extends FilestackIOException {
     public PolicySignatureException() {
         super("Your policy or signature was rejected for the action");
     }
+
+    public PolicySignatureException(String message) {
+        super(message);
+    }
 }
