d096e98c9bf4375ffc8a66c8045466a298f77054
Shawn Aten <shawnmaten@gmail.com>
2017-07-05 16:08:39 -0500

    Add TransformTask subclass for image transforms

---
+++ src/main/java/model/transform/ImageTransformTask.java
@@ -0,0 +1,15 @@
+package model.transform;
+
+/**
+ * Base class for image transformation tasks.
+ */
+public class ImageTransformTask extends TransformTask {
+
+    ImageTransformTask() {
+    }
+
+    ImageTransformTask(String name) {
+        super(name);
+    }
+}
+
