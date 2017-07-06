160080ff2a00945a6aaa84196a985ec3fa444421
Shawn Aten <shawnmaten@gmail.com>
2017-07-06 12:50:52 -0500

    Remove temporary resize method

--- src/main/java/model/transform/ImageTransform.java
+++ src/main/java/model/transform/ImageTransform.java
@@ -56,15 +56,4 @@ public class ImageTransform extends Transform {
         tasks.add(task);
         return this;
     }
-
-    // TODO This is just for demonstration, it should be confirmed when real transforms are added
-    public ImageTransform resize(Integer width, Integer height, String fit, String align) {
-        TransformTask task = new TransformTask("resize");
-        task.addOption("width", width);
-        task.addOption("height", height);
-        task.addOption("fit", fit);
-        task.addOption("align", align);
-        tasks.add(task);
-        return this;
-    }
 }
