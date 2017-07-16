bfade357691793be8888489e4d2aa1836254e5d8
Shawn Aten <shawnmaten@gmail.com>
2017-07-16 17:28:08 -0500

    Add borders and effects tasks

---
+++ src/test/java/model/transform/tasks/TestPolaroidTask.java
@@ -0,0 +1,28 @@
+package model.transform.tasks;
+
+import model.transform.base.TransformTask;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestPolaroidTask {
+
+    @Test
+    public void testToString() {
+        String correct = "polaroid="
+                + "color:white,"
+                + "rotate:90,"
+                + "background:black";
+
+        TransformTask task = new PolaroidTask.Builder()
+                .color("white")
+                .rotate(90)
+                .background("black")
+                .build();
+
+        String output = task.toString();
+
+        String message = String.format("Task string malformed\nCorrect: %s\nOutput: %s", correct, output);
+        assertTrue(message, output.equals(correct));
+    }
+}
---
+++ src/test/java/model/transform/tasks/TestVignetteTask.java
@@ -0,0 +1,28 @@
+package model.transform.tasks;
+
+import model.transform.base.TransformTask;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestVignetteTask {
+
+    @Test
+    public void testToString() {
+        String correct = "vignette="
+                + "amount:50,"
+                + "blurmode:linear,"
+                + "background:white";
+
+        TransformTask task = new VignetteTask.Builder()
+                .amount(50)
+                .blurMode("linear")
+                .background("white")
+                .build();
+
+        String output = task.toString();
+
+        String message = String.format("Task string malformed\nCorrect: %s\nOutput: %s", correct, output);
+        assertTrue(message, output.equals(correct));
+    }
+}
