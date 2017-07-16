f7a1ddb287ecb8fa8766f5b212c1f88072461782
Shawn Aten <shawnmaten@gmail.com>
2017-07-16 16:23:04 -0500

    Add rotate tasks

---
+++ src/test/java/model/transform/tasks/TestFlopTask.java
@@ -0,0 +1,21 @@
+package model.transform.tasks;
+
+import model.transform.base.TransformTask;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestFlopTask {
+
+    @Test
+    public void testToString() {
+        String correct = "flop";
+
+        TransformTask task = new FlopTask();
+
+        String output = task.toString();
+
+        String message = String.format("Task string malformed\nCorrect: %s\nOutput: %s", correct, output);
+        assertTrue(message, output.equals(correct));
+    }
+}
