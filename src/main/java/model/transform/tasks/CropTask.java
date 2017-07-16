d5b427c8582dddf44f1f178569d2d4e7780fa1aa
Shawn Aten <shawnmaten@gmail.com>
2017-07-16 16:16:03 -0500

    Add crop task

---
+++ src/main/java/model/transform/tasks/CropTask.java
@@ -0,0 +1,13 @@
+package model.transform.tasks;
+
+import model.transform.base.ImageTransformTask;
+
+public class CropTask extends ImageTransformTask {
+
+    // Constructor left public because this task must be created with all these options
+    // Builder doesn't make sense for this task
+    public CropTask(int x, int y, int width, int height) {
+        super("crop");
+        addOption("dim", new Integer[]{x, y, width, height});
+    }
+}
