83c0799a6cd2411eb3ba84e16ff5f6f93002513c
Shawn Aten <shawnmaten@gmail.com>
2017-07-17 11:10:52 -0500

    Add ascii task

---
+++ src/main/java/model/transform/tasks/AsciiTask.java
@@ -0,0 +1,48 @@
+package model.transform.tasks;
+
+import model.transform.base.ImageTransformTask;
+
+public class AsciiTask extends ImageTransformTask {
+
+    // Constructor left public because this task can be used with default options
+    public AsciiTask() {
+        super("ascii");
+    }
+
+    public static class Builder {
+        private AsciiTask asciiTask;
+
+        public Builder() {
+            this.asciiTask = new AsciiTask();
+        }
+
+        public Builder background(String background) {
+            asciiTask.addOption("background", background);
+            return this;
+        }
+
+        public Builder foreground(String foreground) {
+            asciiTask.addOption("foreground", foreground);
+            return this;
+        }
+
+        public Builder colored(boolean colored) {
+            asciiTask.addOption("colored", colored);
+            return this;
+        }
+
+        public Builder size(int size) {
+            asciiTask.addOption("size", size);
+            return this;
+        }
+
+        public Builder reverse(boolean reverse) {
+            asciiTask.addOption("reverse", reverse);
+            return this;
+        }
+
+        public AsciiTask build() {
+            return asciiTask;
+        }
+    }
+}
