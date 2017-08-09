8cd7d2b413eb8da426a658b1e0bdbdb6741ba500
Shawn Aten <shawnmaten@gmail.com>
2017-08-09 16:14:38 -0500

    Fix linter warnings for Transform

--- src/main/java/com/filestack/model/transform/base/Transform.java
+++ src/main/java/com/filestack/model/transform/base/Transform.java
@@ -1,17 +1,14 @@
 package com.filestack.model.transform.base;
 
-import com.filestack.model.FilestackClient;
 import com.filestack.model.FileLink;
+import com.filestack.model.FilestackClient;
 import com.filestack.model.Security;
-import okhttp3.HttpUrl;
 import com.filestack.util.FilestackService;
 import com.filestack.util.Networking;
-
 import java.util.ArrayList;
+import okhttp3.HttpUrl;
 
-/**
- * Base class for file transformations and conversions.
- */
+/** Base class for file transformations and conversions. */
 public class Transform {
     String apiKey;
     String source;
@@ -50,31 +47,38 @@ public class Transform {
         }
     }
 
-    /**
-     * Build tasks into single string to insert into request.
-     */
+    /** Format tasks into single string to insert into request. */
     protected String getTasksString() {
-        if (tasks.size() == 0)
+        if (tasks.size() == 0) {
             return "";
+        }
 
         StringBuilder stringBuilder = new StringBuilder();
-        for (TransformTask task : tasks)
+        for (TransformTask task : tasks) {
             stringBuilder.append(task.toString()).append('/');
-        stringBuilder.deleteCharAt(stringBuilder.length()-1);
+        }
+        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
         return stringBuilder.toString();
     }
 
+    /**
+     * Generates a URL of the transformation.
+     * Includes the related {@link FileLink FileLink's} policy and signature.
+     *
+     * @return transformation URL
+     */
     public String url() {
         String tasksString = getTasksString();
         HttpUrl httpUrl;
 
-        if (apiKey != null)
+        if (apiKey != null) {
             httpUrl = processService.getExternal(apiKey, tasksString, source).request().url();
-        else
+        } else {
             httpUrl = processService.get(tasksString, source).request().url();
+        }
 
-        // When forming the request we add a / between tasks, then add that entire string as a path variable
-        // Because it's added as a single path variable, the / is URL encoded
+        // When building the request we add a / between tasks
+        // Because that entire task string is added as a single path variable, the / is URL encoded
         // That's a little confusing so we're replacing "%2F" with "/" for a more expected URL
         return httpUrl.toString().replace("%2F", "/");
     }
