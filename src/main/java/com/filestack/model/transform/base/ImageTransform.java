1220667f5dc74df54a65974984fc2abd6736a58f
Shawn Aten <shawnmaten@gmail.com>
2017-08-09 15:57:17 -0500

    Fix linter warnings for ImageTransform

--- src/main/java/com/filestack/model/transform/base/ImageTransform.java
+++ src/main/java/com/filestack/model/transform/base/ImageTransform.java
@@ -1,11 +1,10 @@
 package com.filestack.model.transform.base;
 
-import com.filestack.model.FilestackClient;
-import com.google.gson.JsonObject;
 import com.filestack.model.FileLink;
+import com.filestack.model.FilestackClient;
 import com.filestack.model.transform.tasks.StoreOptions;
 import com.filestack.util.FilestackService;
-
+import com.google.gson.JsonObject;
 import java.io.IOException;
 
 /**
@@ -21,39 +20,63 @@ public class ImageTransform extends Transform {
         super(fileLink);
     }
 
+    /**
+     * Debugs the transformation as built so far, returning explanations of any issues.
+     *
+     * @return {@link JsonObject JSON} report for transformation
+     * @throws IOException if a network error occurs, handle doesn't exist, or security is invalid
+     */
     public JsonObject debug() throws IOException {
         String tasksString = getTasksString();
 
-        if (apiKey != null)
+        if (apiKey != null) {
             return processService.debugExternal(apiKey, tasksString, source).execute().body();
-        else
+        } else {
             return processService.debug(tasksString, source).execute().body();
+        }
     }
 
     public FileLink store() throws IOException {
         return store(null);
     }
 
+    /**
+     * Stores the result of a transformation into a new file.
+     * https://www.filestack.com/docs/image-transformations/store
+     *
+     * @param storeOptions configure where and how your file is stored
+     * @return new {@link FileLink FileLink} pointing to the file
+     * @throws IOException if a network error occurs, handle doesn't exist, or security is invalid
+     */
     public FileLink store(StoreOptions storeOptions) throws IOException {
-        if (storeOptions == null)
+        if (storeOptions == null) {
             storeOptions = new StoreOptions();
+        }
         addTask(storeOptions);
 
         FilestackService.Process.StoreResponse response;
         String tasksString = getTasksString();
 
-        if (apiKey != null)
+        if (apiKey != null) {
             response = processService.storeExternal(apiKey, tasksString, source).execute().body();
-        else
+        } else {
             response = processService.store(tasksString, source).execute().body();
+        }
 
         String handle = response.getUrl().split("/")[3];
         return new FileLink(apiKey, handle, security);
     }
 
+    /**
+     * Add a new transformation to the chain.
+     * Tasks are executed in the order they are added.
+     *
+     * @param task any one of the available {@link ImageTransformTask ImageTransformTask} subclasses
+     */
     public ImageTransform addTask(ImageTransformTask task) {
-        if (task == null)
+        if (task == null) {
             throw new IllegalArgumentException("Cannot add null task to image transform");
+        }
         tasks.add(task);
         return this;
     }
