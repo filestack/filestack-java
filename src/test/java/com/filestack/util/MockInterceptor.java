65e3068344cb428e758fda1162a2b73382594075
Shawn Aten <shawnmaten@gmail.com>
2017-08-13 18:31:54 -0500

    Fix linter warnings for MockInterceptor

--- src/test/java/com/filestack/util/MockInterceptor.java
+++ src/test/java/com/filestack/util/MockInterceptor.java
@@ -1,13 +1,37 @@
 package com.filestack.util;
 
-import com.google.gson.JsonObject;
-import okhttp3.*;
+import static com.filestack.util.MockConstants.CDN_MOCK_CONTENT;
+import static com.filestack.util.MockConstants.CDN_MOCK_FILENAME;
+import static com.filestack.util.MockConstants.CODE_BAD_REQUEST;
+import static com.filestack.util.MockConstants.CODE_FORBIDDEN;
+import static com.filestack.util.MockConstants.CODE_NOT_FOUND;
+import static com.filestack.util.MockConstants.CODE_OK;
+import static com.filestack.util.MockConstants.HEADER_FILENAME;
+import static com.filestack.util.MockConstants.MESSAGE_BAD_REQUEST;
+import static com.filestack.util.MockConstants.MESSAGE_FORBIDDEN;
+import static com.filestack.util.MockConstants.MESSAGE_NOT_FOUND;
+import static com.filestack.util.MockConstants.MESSAGE_OK;
+import static com.filestack.util.MockConstants.MIME_JSON;
+import static com.filestack.util.MockConstants.MIME_TEXT;
+import static com.filestack.util.MockConstants.MOCK_BASE_URL;
+import static com.filestack.util.MockConstants.TEST_BAD_REQUEST_PATH;
+import static com.filestack.util.MockConstants.TEST_FORBIDDEN_PATH;
+import static com.filestack.util.MockConstants.TEST_HEADER_PATH;
+import static com.filestack.util.MockConstants.TEST_NOT_FOUND_PATH;
+import static com.filestack.util.MockConstants.TEST_UNMATCHED_PATH;
 
+import com.google.gson.JsonObject;
 import java.io.IOException;
 import java.util.Date;
 import java.util.List;
-
-import static com.filestack.util.MockConstants.*;
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
+import okhttp3.MediaType;
+import okhttp3.Protocol;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 /**
  * Intercepts HTTP requests and returns mock responses for unit testing.
@@ -23,16 +47,17 @@ public class MockInterceptor implements Interceptor {
         String host = url.host();
         List<String> path = url.pathSegments();
 
-        if (FilestackService.Cdn.URL.contains(host))
+        if (FilestackService.Cdn.URL.contains(host)) {
             builder = genCdnResponse(path.get(0));
-        else if (FilestackService.Api.URL.contains(host))
+        } else if (FilestackService.Api.URL.contains(host)) {
             builder = genApiResponse(request);
-        else if (FilestackService.Process.URL.contains(host))
+        } else if (FilestackService.Process.URL.contains(host)) {
             builder = genProcessResponse(request);
-        else if (MOCK_BASE_URL.contains(host))
+        } else if (MOCK_BASE_URL.contains(host)) {
             builder = genSimpleResponse(path.get(0));
-        else
+        } else {
             throw new FilestackException("Host not mocked");
+        }
 
         return builder
                 .protocol(Protocol.HTTP_1_1)
@@ -73,11 +98,12 @@ public class MockInterceptor implements Interceptor {
 
                 // This is the only checking we're doing for the overwrite request
                 // Make sure we didn't send an empty body, shouldn't be possible
-                if (request.body() == null)
+                if (request.body() == null) {
                     return new Response.Builder()
                             .code(CODE_BAD_REQUEST)
                             .message(MESSAGE_BAD_REQUEST)
                             .body(responseBody);
+                }
 
                 return new Response.Builder().code(CODE_OK).message(MESSAGE_OK).body(responseBody);
 
@@ -119,8 +145,10 @@ public class MockInterceptor implements Interceptor {
             storeResponse.addProperty("height", "1000");
             storeResponse.addProperty("width", "1000");
             storeResponse.addProperty("size", "200000");
-            storeResponse.addProperty("url", "https://cdn.filestackcontent.com/NEW_HANDLE");
-            responseBody = ResponseBody.create(MediaType.parse(MIME_JSON), storeResponse.toString());
+            storeResponse.addProperty("url",
+                    "https://cdn.filestackcontent.com/NEW_HANDLE");
+            responseBody = ResponseBody.create(MediaType.parse(MIME_JSON),
+                    storeResponse.toString());
 
             // Always return a successful response to this endpoint
             return new Response.Builder().code(CODE_OK).message(MESSAGE_OK).body(responseBody);
