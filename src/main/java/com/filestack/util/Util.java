5ad27b03bdb7fe2ceb4375ce157275294381b139
Shawn Aten <shawnmaten@gmail.com>
2017-08-17 17:51:50 -0500

    Add utility methods for new exception classes

--- src/main/java/com/filestack/util/Util.java
+++ src/main/java/com/filestack/util/Util.java
@@ -1,11 +1,18 @@
 package com.filestack.util;
 
+import com.filestack.errors.FilestackException;
+import com.filestack.errors.InternalException;
+import com.filestack.errors.InvalidParameterException;
+import com.filestack.errors.PolicySignatureException;
+import com.filestack.errors.ResourceNotFoundException;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
+import retrofit2.Response;
 
 /**
  * Small helper functions that don't need their own class.
@@ -39,5 +46,56 @@ public class Util {
    */
   public static RequestBody createStringPart(String content) {
     return RequestBody.create(MultipartBody.FORM, content);
+  }
+
+  /**
+   * Checks responses from any of the Filestack APIs.
+   * Throws an appropriate exception based on the HTTP status code.
+   *
+   * @param response response from Filestack API
+   * @throws InvalidParameterException on a 400 response
+   * @throws PolicySignatureException  on a 403 response
+   * @throws ResourceNotFoundException on a 404 response
+   * @throws InternalException  on a 500 response
+   */
+  public static void checkResponseAndThrow(Response response)
+      throws InvalidParameterException, PolicySignatureException, ResourceNotFoundException,
+      InternalException {
+
+    int code = response.code();
+
+    if (code == 400) {
+      throw new InvalidParameterException();
+    } else if (code == 403) {
+      throw new PolicySignatureException();
+    } else if (code == 404) {
+      throw new ResourceNotFoundException();
+    } else if (code >= 500) {
+      throw new InternalException();
+    }
+  }
+
+  /**
+   * Takes an {@link Exception} and recasts it to one of the {@link FilestackException} classes.
+   *
+   * @param e generic object to cast and rethrow
+   */
+  public static void castExceptionAndThrow(Exception e)
+      throws InvalidParameterException, IOException, PolicySignatureException,
+             ResourceNotFoundException, InternalException {
+
+    if (e instanceof InvalidParameterException) {
+      throw (InvalidParameterException) e;
+    } else if (e instanceof IOException) {
+      throw (IOException) e;
+    } else if (e instanceof PolicySignatureException) {
+      throw (PolicySignatureException) e;
+    } else if (e instanceof ResourceNotFoundException) {
+      throw (ResourceNotFoundException) e;
+    } else if (e instanceof  InternalException) {
+      throw (InternalException) e;
+    }
+
+    throw new InternalException(e);
   }
 }
