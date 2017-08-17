7d6bbc601b12c0b87d71243ad1fe22634064b77f
Shawn Aten <shawnmaten@gmail.com>
2017-08-17 17:53:22 -0500

    Update ImageTransform with new exception classes

--- src/main/java/com/filestack/transforms/ImageTransform.java
+++ src/main/java/com/filestack/transforms/ImageTransform.java
@@ -2,13 +2,19 @@ package com.filestack.transforms;
 
 import com.filestack.FileLink;
 import com.filestack.FilestackClient;
+import com.filestack.errors.InternalException;
+import com.filestack.errors.InvalidParameterException;
+import com.filestack.errors.PolicySignatureException;
+import com.filestack.errors.ResourceNotFoundException;
 import com.filestack.responses.StoreResponse;
 import com.filestack.transforms.tasks.StoreOptions;
+import com.filestack.util.Util;
 import com.google.gson.JsonObject;
 import io.reactivex.Single;
 import io.reactivex.schedulers.Schedulers;
 import java.io.IOException;
 import java.util.concurrent.Callable;
+import retrofit2.Response;
 
 /**
  * {@link Transform Transform} subclass for image transformations.
@@ -25,60 +31,104 @@ public class ImageTransform extends Transform {
 
   /**
    * Debugs the transformation as built so far, returning explanations of any issues.
+   * @see <a href="https://www.filestack.com/docs/image-transformations/debug"></a>
    *
    * @return {@link JsonObject JSON} report for transformation
-   * @throws IOException for network failures, invalid handles, or invalid security
+   * @throws IOException               if request fails because of network or other IO issue
+   * @throws PolicySignatureException  if policy and/or signature are invalid or inadequate
+   * @throws ResourceNotFoundException if API key, handle, or external URL are not found
+   * @throws InvalidParameterException if a request parameter is missing or invalid
+   * @throws InternalException         if unexpected error occurs
    */
-  public JsonObject debug() throws IOException {
+  public JsonObject debug()
+      throws IOException, PolicySignatureException, ResourceNotFoundException,
+             InvalidParameterException, InternalException {
+
     String tasksString = getTasksString();
 
+    Response<JsonObject> response;
     if (apiKey != null) {
-      return fsService.transformDebugExt(apiKey, tasksString, source).execute().body();
+      response = fsService.transformDebugExt(apiKey, tasksString, source).execute();
     } else {
-      return fsService.transformDebug(tasksString, source).execute().body();
+      response = fsService.transformDebug(tasksString, source).execute();
     }
+
+    Util.checkResponseAndThrow(response);
+
+    JsonObject body = response.body();
+    if (body == null) {
+      throw new IOException();
+    }
+
+    return body;
   }
 
-  public FileLink store() throws IOException {
+  /**
+   * Stores the result of a transformation into a new file. Uses default storage options.
+   * @see <a href="https://www.filestack.com/docs/image-transformations/store"></a>
+   *
+   * @return new {@link FileLink FileLink} pointing to the file
+   * @throws IOException               if request fails because of network or other IO issue
+   * @throws PolicySignatureException  if policy and/or signature are invalid or inadequate
+   * @throws ResourceNotFoundException if API key, handle, or external URL are not found
+   * @throws InvalidParameterException if a request parameter is missing or invalid
+   * @throws InternalException         if unexpected error occurs
+   */
+  public FileLink store()
+      throws IOException, PolicySignatureException, ResourceNotFoundException,
+             InvalidParameterException, InternalException {
+
     return store(null);
   }
 
   /**
    * Stores the result of a transformation into a new file.
-   * https://www.filestack.com/docs/image-transformations/store
+   * @see <a href="https://www.filestack.com/docs/image-transformations/store"></a>
    *
    * @param storeOptions configure where and how your file is stored
    * @return new {@link FileLink FileLink} pointing to the file
-   * @throws IOException for network failures, invalid handles, or invalid security
+   * @throws IOException               if request fails because of network or other IO issue
+   * @throws PolicySignatureException  if policy and/or signature are invalid or inadequate
+   * @throws ResourceNotFoundException if API key, handle, or external URL are not found
+   * @throws InvalidParameterException if a request parameter is missing or invalid
+   * @throws InternalException         if unexpected error occurs
    */
-  public FileLink store(StoreOptions storeOptions) throws IOException {
+  public FileLink store(StoreOptions storeOptions)
+      throws IOException, PolicySignatureException, ResourceNotFoundException,
+             InvalidParameterException, InternalException {
+
     if (storeOptions == null) {
       storeOptions = new StoreOptions();
     }
     addTask(storeOptions);
 
-    StoreResponse response;
+    Response<StoreResponse> response;
     String tasksString = getTasksString();
-
     if (apiKey != null) {
-      response = fsService.transformStoreExt(apiKey, tasksString, source).execute().body();
+      response = fsService.transformStoreExt(apiKey, tasksString, source).execute();
     } else {
-      response = fsService.transformStore(tasksString, source).execute().body();
+      response = fsService.transformStore(tasksString, source).execute();
     }
 
-    String handle = response.getUrl().split("/")[3];
+    Util.checkResponseAndThrow(response);
+
+    StoreResponse body = response.body();
+    if (body == null) {
+      throw new IOException();
+    }
+
+    String handle = body.getUrl().split("/")[3];
     return new FileLink(apiKey, handle, security);
   }
 
   /**
-   * Add a new transformation to the chain.
-   * Tasks are executed in the order they are added.
+   * Add a new transformation to the chain. Tasks are executed in the order they are added.
    *
-   * @param task any one of the available {@link ImageTransformTask ImageTransformTask} subclasses
+   * @param task any of the available {@link ImageTransformTask} subclasses
    */
   public ImageTransform addTask(ImageTransformTask task) {
     if (task == null) {
-      throw new IllegalArgumentException("Cannot add null task to image transform");
+      throw new NullPointerException("Cannot add null task to image transform");
     }
     tasks.add(task);
     return this;
@@ -88,7 +138,7 @@ public class ImageTransform extends Transform {
 
   /**
    * Async, observable version of {@link #debug()}.
-   * Throws same exceptions.
+   * Same exceptions are passed through observable.
    */
   public Single<JsonObject> debugAsync() {
     return Single.fromCallable(new Callable<JsonObject>() {
@@ -103,7 +153,7 @@ public class ImageTransform extends Transform {
 
   /**
    * Async, observable version of {@link #store()}.
-   * Throws same exceptions.
+   * Same exceptions are passed through observable.
    */
   public Single<FileLink> storeAsync() {
     return storeAsync(null);
@@ -111,7 +161,7 @@ public class ImageTransform extends Transform {
 
   /**
    * Async, observable version of {@link #store(StoreOptions)}.
-   * Throws same exceptions.
+   * Same exceptions are passed through observable.
    */
   public Single<FileLink> storeAsync(final StoreOptions storeOptions) {
     return Single.fromCallable(new Callable<FileLink>() {
