12239061a8ab720ad9b1ac5247988076a720ab30
Shawn Aten <shawnmaten@gmail.com>
2017-08-27 17:14:36 -0500

    Add NoMetadataOption

---
+++ src/main/java/com/filestack/transforms/tasks/NoMetadataOption.java
@@ -0,0 +1,10 @@
+package com.filestack.transforms.tasks;
+
+import com.filestack.transforms.ImageTransformTask;
+
+public class NoMetadataOption extends ImageTransformTask {
+
+  public NoMetadataOption() {
+    super("no_metadata");
+  }
+}
