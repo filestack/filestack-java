f8d75e9c640283d3d89a9647253725678743a76d
Shawn Aten <shawnmaten@gmail.com>
2017-09-09 18:59:07 -0500

    Flesh out Progress class

--- src/main/java/com/filestack/Progress.java
+++ src/main/java/com/filestack/Progress.java
@@ -1,18 +1,55 @@
 package com.filestack;
 
 public class Progress<T> {
-  private int bytes;
-  private T data;
+  private static final double SMOOTHING_FACTOR = 0.005;
 
-  public Progress(int bytes, T data) {
-    this.bytes = bytes;
+  private final long bytesMoved;
+  private final long bytesTotal;
+  private final int elapsed;
+  private final double rate;
+  private final T data;
+
+  public Progress(long bytesMoved, long bytesTotal, int elapsed, double rate, T data) {
+    this.bytesMoved = bytesMoved;
+    this.bytesTotal = bytesTotal;
+    this.elapsed = elapsed;
+    this.rate = rate;
     this.data = data;
   }
 
-  public int getBytes() {
-    return bytes;
+  /** Calculates exponential moving average. */
+  public static double calcAvg(double lastRate, double avgRate) {
+    return SMOOTHING_FACTOR * lastRate + (1 - SMOOTHING_FACTOR) * avgRate;
   }
 
+  public long getBytesMoved() {
+    return bytesMoved;
+  }
+
+  public long getBytesTotal() {
+    return bytesTotal;
+  }
+
+  public double getPercent() {
+    return bytesMoved / (double) bytesTotal;
+  }
+
+  /** Time elapsed in seconds. */
+  public int getElapsed() {
+    return elapsed;
+  }
+
+  /** Average transfer rate in bytes/second. */
+  public double getRate() {
+    return rate;
+  }
+
+  /** Estimated time left in seconds given current progress and rate. */
+  public int getEta() {
+    return (int) Math.ceil((bytesTotal - bytesMoved) / rate);
+  }
+
+  /** Get any data associated with the transfer. May be null. */
   public T getData() {
     return data;
   }
