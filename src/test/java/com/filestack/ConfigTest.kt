3102f2a590801deb115a0d1a8ed26d4a716b60e8
scana <marcin@qualaroo.com>
2018-09-07 16:49:11 +0200

    Test hasSecurity method with Kotlin

---
+++ src/test/java/com/filestack/ConfigTest.kt
@@ -0,0 +1,31 @@
+package com.filestack
+
+import org.junit.Assert
+import org.junit.Assert.*
+import org.junit.Test
+
+class ConfigTest {
+
+    @Test
+    fun `verifies security`() {
+        var config = Config("api_key")
+        assertFalse(config.hasSecurity())
+
+        config = Config("api_key", "return_url", "policy", null)
+        assertFalse(config.hasSecurity())
+
+        config = Config("api_key", "return_url", null, "policy")
+        assertFalse(config.hasSecurity())
+
+        config = Config("api_key", "return_url", "policy", "signature")
+        assertTrue(config.hasSecurity())
+
+        val policy = Policy.Builder()
+                .giveFullAccess()
+                .build("api_key")
+
+        config = Config("api_key", policy)
+        assertTrue(config.hasSecurity())
+    }
+
+}
