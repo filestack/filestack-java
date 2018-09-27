fd5e399ceb2baba56fa555dd8eb22b4481d11955
Marcin Robaczyński <scanarch@gmail.com>
2018-09-27 15:14:17 +0200

    Enable TLS 1.2 support for pre-KitKat Android devices (#79)
    -- this change is required in order to make network requests work on pre Kitkat devices
    -- take a look here: https://github.com/square/okhttp/issues/2372

---
+++ src/main/java/com/filestack/internal/TlsSocketFactory.java
@@ -0,0 +1,60 @@
+package com.filestack.internal;
+
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
+class TlsSocketFactory extends SSLSocketFactory {
+
+  private static final String[] TLS_1_2 = {"TLSv1.2"};
+
+  private final SSLSocketFactory factory;
+
+  TlsSocketFactory(SSLSocketFactory factory) {
+    this.factory = factory;
+  }
+
+  @Override
+  public String[] getDefaultCipherSuites() {
+    return factory.getDefaultCipherSuites();
+  }
+
+  @Override
+  public String[] getSupportedCipherSuites() {
+    return factory.getSupportedCipherSuites();
+  }
+
+  @Override
+  public Socket createSocket(Socket socket, String s, int i, boolean b) throws IOException {
+    return patch(factory.createSocket(socket, s, i, b));
+  }
+
+  @Override
+  public Socket createSocket(String s, int i) throws IOException {
+    return patch(factory.createSocket(s, i));
+  }
+
+  @Override
+  public Socket createSocket(String s, int i, InetAddress inetAddress, int i1) throws IOException {
+    return patch(factory.createSocket(s, i, inetAddress, i1));
+  }
+
+  @Override
+  public Socket createSocket(InetAddress inetAddress, int i) throws IOException {
+    return patch(factory.createSocket(inetAddress, i));
+  }
+
+  @Override
+  public Socket createSocket(InetAddress inetAddress, int i, InetAddress inetAddress1, int i1) throws IOException {
+    return patch(factory.createSocket(inetAddress, i, inetAddress1, i1));
+  }
+
+  private Socket patch(Socket s) {
+    if (s instanceof SSLSocket) {
+      ((SSLSocket) s).setEnabledProtocols(TLS_1_2);
+    }
+    return s;
+  }
+}
